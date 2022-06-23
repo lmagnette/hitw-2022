24082933bc6ef6c93a88b8274827894fb88b3989
lma <loic.magnette@oniryx.be>
2022-06-23 15:21:15 +0200

    configure capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'be.oniryx.hitw2022',
+  appName: 'hitw-2022',
+  webDir: 'dist/hitw-2022',
+  bundledWebRuntime: false
+};
+
+export default config;
